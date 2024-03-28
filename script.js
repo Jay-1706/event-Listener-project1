4d5f200b953daab33a3989535fe0ca54c7011135
Jay-1706 <125432967+Jay-1706@users.noreply.github.com>
2024-03-28 22:50:30 +0530

    Add files via upload
    eventListener-Doubleclick

---
+++ script.js
@@ -0,0 +1,5 @@
+ const boxelement=document.getElementById("box");
+ //add a click event listener
+ boxelement.addEventListener("dblclick",function(){
+    alert("Namste India❤️‍🔥,u clicked twice")
+ })
